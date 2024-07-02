d7b89f1fdb2ffbe03d44762b20d29eec1e96fb22
AiJiangnan <AiJiangnan@users.noreply.github.com>
2024-07-02 01:23:12 +0000

    deploy: 722f3474684dd846e9ba08037fde5cc1a06c52dc

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/school-tools/sw.js', { scope: '/school-tools/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
